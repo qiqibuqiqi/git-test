64488e91f5181a5ee25420bc7a7ee48a00deb9b5
xzc <xzc@x.com>
2022-08-30 12:27:07 +0800

    hot-fix 1

--- src/main/java/com/x/git/GitTest.java
+++ src/main/java/com/x/git/GitTest.java
@@ -5,5 +5,6 @@ public class GitTest {
         System.out.println("Hello git!");
         System.out.println("Hello git!2");
         System.out.println("Hello git!3");
+        System.out.println("Hello git!4");
     }
 }
